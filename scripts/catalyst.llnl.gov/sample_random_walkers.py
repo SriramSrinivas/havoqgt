bff891fd268226a54dd3cf8c16faed6e4056adde
Olivia Simpson <oliviamsimpson@gmail.com>
2017-01-26 10:46:13 -0800

    comments

--- scripts/catalyst.llnl.gov/sample_random_walkers.py
+++ scripts/catalyst.llnl.gov/sample_random_walkers.py
@@ -25,8 +25,9 @@ T = end_time-start_time
 r2 = 2.0*(T/tau)*(eps**(-2))*np.log(eps**(-1))
 print("sampling", str(r2), "times")
 
+# simulate R1 random walkers
 r1 = int((12/(eps**2*delta))*np.log(eps**(-1)))
-print("sampling", str(r1), "random walkers")
+print("simulating", str(r1), "random walkers")
 
 os.chdir(os.path.dirname('/g/g17/osimpson/evolving/havoqgt/build/catalyst.llnl.gov/'))
 os.system( 'srun -N32 --ntasks-per-node=24 --distribution=block ./src/run_random_walk_simulation -i /l/ssd/output -m /l/ssd/metadata -c 10000 -n ' + str(r1) + ' -a brad_pitt_id.txt -s ' + str(start_time) + ' -e ' + str(end_time) + ' -r /p/lscratchf/osimpson/output -d 15 -t ' + str(r2) )
