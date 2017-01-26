11bd07cfee040c9db3b39970d46ab2b6139f5945
Olivia Simpson <oliviamsimpson@gmail.com>
2017-01-26 12:22:46 -0800

    whoops put back the right do_cmake

--- scripts/catalyst.llnl.gov/do_cmake.sh
+++ scripts/catalyst.llnl.gov/do_cmake.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
+ 
+ 
 INSTALL_PREFIX=${PWD}
-
+ 
+ 
 rm CMakeCache.txt
-
+  
 cmake ../../ \
   -DHAVOQGT_BUILD_TEST=TRUE \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
   -DCMAKE_BUILD_TYPE=Release \
   -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicc \
   -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicxx \
-  -DBOOST_ROOT=/usr/tce/packages/boost/boost-1.62.0-mvapich2-2.2-gcc-4.9.3/ \
-  -DCMAKE_CXX_FLAGS="-std=c++11 -lrt -lpthread -lm" \
+  -DBOOST_ROOT=/usr/gapps/dst/opt/boost_stages/boost_1_56_0/ \
+  -DCMAKE_CXX_FLAGS="-std=c++11 -lrt" \
