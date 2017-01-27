b980567601fcf24d68080394f812dbc10fcaf230
Tahsin Arafat Reza <reza2@llnl.gov>
2017-01-27 12:59:23 -0800

    cmake script updates following catalyst TOSS3 upgrade

--- scripts/catalyst.llnl.gov/do_cmake.sh
+++ scripts/catalyst.llnl.gov/do_cmake.sh
@@ -13,4 +13,4 @@ cmake ../../ \
   -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicc \
   -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicxx \
   -DBOOST_ROOT=/usr/gapps/dst/opt/boost_stages/boost_1_56_0/ \
-  -DCMAKE_CXX_FLAGS="-std=c++11 -lrt" \
+  -DCMAKE_CXX_FLAGS="-std=c++11 -lrt -lpthread -lm" \
