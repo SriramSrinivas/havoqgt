8582c8390d3eae83efd664c2138364c1086f99da
Olivia Simpson <oliviamsimpson@gmail.com>
2017-01-30 09:06:40 -0800

    local boost from Tahsin

--- scripts/catalyst.llnl.gov/do_cmake_debug.sh
+++ scripts/catalyst.llnl.gov/do_cmake_debug.sh
@@ -12,7 +12,7 @@ cmake ../../ \
   -DCMAKE_BUILD_TYPE=Debug \
   -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicc \
   -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicxx \
-  -DBOOST_ROOT=/usr/gapps/dst/opt/boost_stages/boost_1_56_0/ \
+  -DBOOST_ROOT=/g/g17/osimpson/boost_1_57_0/ \
   -DCMAKE_CXX_FLAGS="-std=c++11 -Wredundant-decls -DDEBUG -DDEBUG_DPG -lrt -lpthread -lm" \
 
 
